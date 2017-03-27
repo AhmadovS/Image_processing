6f287785494ffe214b98a4c7ed7b07a706a5dfc6
Samud Ahmadov <samud.ahmadov@mail.utoronto.ca>
2017-03-27 02:26:17 -0400

    Bug fix

--- CS320/A3/code/algorithm.py
+++ CS320/A3/code/algorithm.py
@@ -95,12 +95,22 @@ def propagation_and_random_search(source_patches, target_patches,
             if not propagation_enabled:
                 ##Odd iterations
                 if odd_iteration:
-                    idxs = np.vstack((new_f[x, y], new_f[x - 1, y], new_f[x, y - 1]))
+                    idxs = [new_f[x, y]]
+                    if x != 0:
+                        idxs.append(new_f[x - 1, y])
+                    if y != 0:
+                        idxs.append(new_f[x, y-1])
+
+                    idxs = np.vstack(idxs)
                 ##Even iterations
                 else:
                     idxs = [new_f[x, y]]
-                    idxs.append(new_f[x + 1, y, :] if x + 1< shp[0] else new_f[0,y,:])
-                    idxs.append(new_f[x, y+1, :] if y + 1 < shp[1] else new_f[x, 0,:])
+                    if x!=shp[0]-1:
+                        idxs.append(new_f[x+1, y, :])
+                    if y!=shp[1]-1:
+                        idxs.append(new_f[x, y+1, :])
+                    # idxs.append(new_f[x + 1, y, :] if x + 1< shp[0] else new_f[0,y,:])
+                    # idxs.append(new_f[x, y+1, :] if y + 1 < shp[1] else new_f[x, 0,:])
                     idxs = np.vstack(idxs)
 
                 # res = []
@@ -113,6 +123,7 @@ def propagation_and_random_search(source_patches, target_patches,
                 idxs[y_bound, 1] -= shp[1] * targets[y_bound, 1] // shp[1]
 
                 all_targets = target_patches[idxs[:, 0] + x, idxs[:, 1] + y]
+                # all_targets = np.rollaxis(all_targets,2,1)
                 # print (all_targets)
                 # print("Salam\n")
                 # print (src_patch)
@@ -133,32 +144,6 @@ def propagation_and_random_search(source_patches, target_patches,
                     best_D[x, y, :] = tmp_min
                     new_f[x, y, :] = idxs[np.argmin(result), :]
 
-                # ##Troubleshooting and find min
-                # for id in idxs:
-                #
-                #     if abs(x + id[0]) >= shp[0]:
-                #         id[0] -= shp[0]*(x + id[0])//shp[0]
-                #     if abs(y + id[1]) >= shp[1]:
-                #         # print(id)
-                #         id[1] -= shp[1]*(y + id[1])//shp[1]
-                #         # print(id)
-                #
-                #     # print('\n')
-                #     # print(id)
-                #     # print(x, y)
-                #     trgt_patch = target_patches[x + id[0], y + id[1], :, :]
-                #     # trgt_patch[np.isnan(trgt_patch)] = 0
-                #     patch_dist = (trgt_patch - src_patch) ** 2
-                #     n_nan = len(patch_dist[~np.isnan(patch_dist)])
-                #
-                #     patch_dist[np.isnan(patch_dist)] = 0
-                #     res.append(np.sqrt(np.sum(patch_dist) / n_nan))
-                #
-                # tmp_D = np.min(res)
-                # if np.isnan(best_D[x, y]) or tmp_D < best_D[x, y]:
-                #     best_D[x, y, :] = tmp_D
-                #
-                # new_f[x, y, :] = idxs[np.argmin(res)]
 
             if not random_enabled:
 
@@ -195,6 +180,8 @@ def propagation_and_random_search(source_patches, target_patches,
                 # #
                 # print (offsets)
                 all_targets = target_patches[offsets[:,0]+x, offsets[:,1]+y]
+                # all_targets = np.rollaxis(all_targets, 2, 1)
+
                 res = (all_targets - src_patch)
 
                 res = res.reshape(res.shape[0], res.shape[1]*res.shape[2])
